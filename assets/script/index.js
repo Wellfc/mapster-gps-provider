46d25efd893fa7228212e1b6df2541babe2123b3
Stefano <stefano.turcarelli@icloud.com>
2023-12-13 14:22:09 -0600

    Fix duplicate marker

--- assets/script/index.js
+++ assets/script/index.js
@@ -6,14 +6,15 @@ const trackButton = document.querySelector("#track-btn");
 const flyButton = document.querySelector("#fly-btn");
 let lat, long;
 
-mapboxgl.accessToken = 'pk.eyJ1Ijoid2VsbGZjIiwiYSI6ImNscTE5azY3eDAzeGwyaXIycTgyMnM0ZW8ifQ.0Bp9MRAu0DmdRpUI8lnDPg';
+mapboxgl.accessToken =
+  "pk.eyJ1Ijoid2VsbGZjIiwiYSI6ImNscTE5azY3eDAzeGwyaXIycTgyMnM0ZW8ifQ.0Bp9MRAu0DmdRpUI8lnDPg";
 
 const map = new mapboxgl.Map({
-  container: 'map', // container ID
+  container: "map", // container ID
   center: [-97.143173, 49.895313], // starting position [lng, lat]
   zoom: 4, // starting zoom
   pitch: 40,
-  style: 'mapbox://styles/mapbox/streets-v12' // style URL
+  style: "mapbox://styles/mapbox/streets-v12", // style URL
 });
 
 function getLocation(position) {
@@ -33,22 +34,35 @@ function goCenter(latitude, longitude) {
   map.flyTo({
     center: [longitude, latitude],
     essential: true, // this animation is considered essential with respect to prefers-reduced-motion
-    zoom: 16
+    zoom: 16,
   });
 }
 
+// Store the current marker
+let currentMarker = null;
+
 // Create a marker on the map
 function createMarker(latitude, longitude) {
+  // If a marker already exists, remove it
+  if (currentMarker) {
+    currentMarker.remove();
+  }
+
   // Create a popup, but don't add it to the map yet.
-  const popup = new mapboxgl.Popup({ offset: 20 }).setText(
-    'Mapster'
-  );
+  const popup = new mapboxgl.Popup({ offset: 20 }).setText("Mapster");
 
   // Create a default Marker and add it to the map.
-  const marker = new mapboxgl.Marker({ color: '#0599ff', scale: 0.6, rotation: 0 })
+  const marker = new mapboxgl.Marker({
+    color: "#0599ff",
+    scale: 0.6,
+    rotation: 0,
+  })
     .setLngLat([longitude, latitude])
-    .setPopup(popup)  // sets a popup on this marker
+    .setPopup(popup) // sets a popup on this marker
     .addTo(map);
+
+  // Update the current marker
+  currentMarker = marker;
 }
 
 // The error/failure callback function
@@ -58,23 +72,23 @@ function errorHandler(error) {
 
 // Location options
 const options = {
-  maximumAge: 0,  // don't use the cached location
+  maximumAge: 0, // don't use the cached location
   timeout: 5000,
-  enableHighAccuracy: true
+  enableHighAccuracy: true,
 };
 
 // Track button click event handler
-trackButton.addEventListener('click', () => {
-  if ('geolocation' in navigator) {
+trackButton.addEventListener("click", () => {
+  if ("geolocation" in navigator) {
     const geo = navigator.geolocation;
     geo.getCurrentPosition(getLocation, errorHandler, options);
   } else {
-    alert('Geolocation API is not supported by your browser');
+    alert("Geolocation API is not supported by your browser");
   }
-  flyButton.style.visibility = 'visible';
+  flyButton.style.visibility = "visible";
 });
 
 // Fly button click event handler
-flyButton.addEventListener('click', () => {
+flyButton.addEventListener("click", () => {
   goCenter(lat, long);
 });
